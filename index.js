44d8dcfc3af8b88774b612e362bae8d9a098710a
Charles <Charlessammarco@gmail.com>
2023-06-25 00:42:46 -0500

    behavior methods

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+        this.speak = () => {return `${name} says meow!`}
+    }
+}
+
+class Dog {
+    constructor(name, sex){
+        console.log(name,sex)
+        this.name = name;
+        this.sex = sex;
+         this.speak = () => {return `${name} says woof!`;} 
+    }
+}
+
+class Bird {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+         (sex === "male") ?  this.speak = () => {return `It's me! ${name}, the parrot!`} : this.speak = () => {return `${name} says squawk!`}
+    }
+}
